a607f5750ee6e195b2e8ca1dcf5cf39f8dfc30b5
Ricardo Amendoeira <ricardo.filipe.amendoeira+github@gmail.com>
2020-12-03 16:23:53 +0000

    improve solution for day 3

--- 2020/day03.hs
+++ 2020/day03.hs
@@ -1,26 +1,21 @@
 main = do
     input <- lines <$> readFile "input/day03.txt"
-    let baseMap = map loadLine input
+    let baseMap = map (map ('#' ==)) input
     print $ head $ treesOnSlopes baseMap [(3, 1)] -- 274
     print $ foldl1 (*) $ treesOnSlopes baseMap [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)] -- 6050183040
 
 type BaseMap = [[Bool]]
 
 treesOnSlopes :: BaseMap -> [(Int, Int)] -> [Int]
-treesOnSlopes baseMap slopes = map (treesEncontered baseMap) slopes
-    where treesEncontered baseMap (speed_x, speed_y) = length $ filter (treeAt baseMap) $ traverseToBottom baseMap (0, 0) (speed_x, speed_y)
+treesOnSlopes baseMap slopes = map treesEncontered slopes
+    where treesEncontered (speed_x, speed_y) = length $ filter (treeAt baseMap) $ traverseToBottom baseMap (0, 0) (speed_x, speed_y)
 
 traverseToBottom :: BaseMap -> (Int, Int) -> (Int, Int) -> [(Int, Int)]
-traverseToBottom baseMap (x, y) (speed_x, speed_y) | y < baseMapHeight = [(x, y)] ++ traverseToBottom baseMap (x + speed_x `mod` baseMapWidth, y + speed_y) (speed_x, speed_y)
-                        | otherwise = []
-                            where baseMapHeight = length baseMap
-                                  baseMapWidth = (length . head) baseMap
-
-loadLine :: String -> [Bool]
-loadLine line = map isTree line
-    where isTree '.' = False
-          isTree '#' = True
+traverseToBottom baseMap (x, y) (speed_x, speed_y) 
+    | y >= length baseMap = []
+    | otherwise = [(x, y)] ++ traverseToBottom baseMap (x + speed_x `mod` baseMapWidth, y + speed_y) (speed_x, speed_y)
+        where baseMapWidth = length (head baseMap)
 
 treeAt :: BaseMap -> (Int, Int) -> Bool
 treeAt baseMap (x, y) = (baseMap !! y) !! (x `mod` baseMapWidth)
-    where baseMapWidth = (length . head) baseMap
+    where baseMapWidth = length (head baseMap)
