b1a7423c2fae1f6e507b3ecb3fd7c39dd2bb47e2
Ricardo Amendoeira <ricardo.filipe.amendoeira+github@gmail.com>
2020-12-06 16:52:50 +0000

    refactor solution for day 6

--- 2020/day06.hs
+++ 2020/day06.hs
@@ -1,15 +1,10 @@
-import qualified Data.Set as Set
+import Data.List
 
 main = do
     input <- lines <$> readFile "input/day06.txt"
     let groups = splitOn "" input
-    print $ sum $ map Set.size $ map positiveAnswersByAnyoneInGroup groups -- 6947
-    print $ sum $ map Set.size $ map positiveAnswersByEntireGroup groups -- 3398
-
-positiveAnswersByAnyoneInGroup groupAnswers = Set.unions (map positiveAnswersByPerson groupAnswers)
-positiveAnswersByEntireGroup groupAnswers = foldl1 Set.intersection (map positiveAnswersByPerson groupAnswers)
-
-positiveAnswersByPerson personAnswers = Set.fromList personAnswers
+    print $ sum $ map (length . foldl1 union) groups -- 6947
+    print $ sum $ map (length . foldl1 intersect) groups -- 3398
 
 splitOn delimiter xs = case break (== delimiter) xs of
     (y, _ : ys) -> y : splitOn delimiter ys
