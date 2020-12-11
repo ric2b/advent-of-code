f65af2223e9e1d37301deb7e644e5d6b882d777f
Ricardo Amendoeira <ricardo.filipe.amendoeira+github@gmail.com>
2020-12-11 20:07:16 +0000

    solution for day 11, faster

--- 2020/day11.hs
+++ 2020/day11.hs
@@ -1,57 +1,33 @@
 {-# LANGUAGE NamedFieldPuns #-}
 
 import qualified Data.Map as M
-import qualified Data.Set as S
 
 import Data.List (find)
 import Data.Maybe (catMaybes)
 
 main = do
     raw_input <- lines <$> readFile "input/day11.txt"
-    -- raw_input <- lines <$> readFile "input/day11.example.txt"
     let initialWaitingAreaState = parseWaitingArea raw_input
-    -- -- mapM_ print raw_input
-    -- -- print $ calculateSeats initialWaitingAreaState
-
-    -- -- print $ occupiedSeats initialWaitingAreaState
-
-    -- let areaStates = (iterate tickSeats initialWaitingAreaState !!)
-
-    -- -- print $ areaStates 0
-    -- print $ areaStates 1
-    -- print $ areaStates 2
-    -- print $ areaStates 3
-    -- print $ areaStates 4
-    -- print $ areaStates 5
-    -- print $ areaStates 6
-
-    -- print $ areaStates 5 == areaStates 6
-    -- print $ tickSeats (areaStates 5) == areaStates 5
     
-    print $ length . occupiedSeats $ untilSteadyState tickSeatsAdjacent initialWaitingAreaState -- 2263
-    print $ length . occupiedSeats $ untilSteadyState tickSeatsSight initialWaitingAreaState -- 
+    print $ length $ M.filter (==True) $ seats $ untilSteadyState tickSeatsAdjacent initialWaitingAreaState -- 2263
+    print $ length $ M.filter (==True) $ seats $ untilSteadyState tickSeatsSight initialWaitingAreaState -- 2002
 
 type Position = (Int, Int)
 
 data WaitingAreaState = WaitingAreaState {
         gridLimit :: Position,
-        floorPositions :: S.Set Position,
-        occupiedSeats :: S.Set Position
+        seats :: M.Map Position Bool
 } deriving (Eq)
 
 instance Show WaitingAreaState where
-    show WaitingAreaState{gridLimit, floorPositions, occupiedSeats} = show [[
+    show WaitingAreaState{gridLimit, seats} = show [[
             c | x <- [0..maxX],
-            let c = if S.member (x, y) floorPositions then '.' 
-                else if S.member (x, y) occupiedSeats then '#' 
-                else 'L'
+            let c = case M.lookup (x, y) seats of 
+                        Nothing -> '.' 
+                        (Just True) -> '#' 
+                        (Just False) -> 'L'
         ] | y <- [0..maxY]]
         where (maxX, maxY) = gridLimit
-
--- createWaitingAreaState gridLimit floorPositions occupiedSeats = 
---     WaitingAreaState {gridLimit = (maxX, maxY), floorPositions, occupiedSeats, seats}
---     where seats = S.fromList [(x, y) | y <- [0..maxY], x <- [0..maxX], S.notMember (x, y) floorPositions]
---     where (maxX, maxY) = gridLimit 
 
 untilSteadyState tickSeats currentState = case currentState == nextState of 
     True -> currentState
@@ -59,23 +35,23 @@ untilSteadyState tickSeats currentState = case currentState == nextState of
     where nextState = tickSeats currentState
 
 tickSeatsSight :: WaitingAreaState -> WaitingAreaState
-tickSeatsSight currentState = currentState{occupiedSeats=newOccupiedSeats}
-    where newOccupiedSeats = S.filter (becomesOccupiedSight currentState) seats
-          seats = calculateSeats currentState
+tickSeatsSight currentState@(WaitingAreaState{seats}) = currentState{seats=newSeatStates}
+    where newSeatStates = M.mapWithKey updateSeat seats
+          updateSeat (x, y) occupied = case occupied of
+                False -> occupiedNeighboursCount == 0
+                True -> occupiedNeighboursCount < 5
+            where occupiedNeighboursCount = length neighbouringSeats
+                  neighbouringSeats = [
+                      neighbourOccupied | p <- (neighboursBySight currentState (x, y)),
+                        let neighbourOccupied = case M.lookup p seats of
+                                Nothing -> False
+                                (Just o) -> o,
+                        neighbourOccupied
+                    ]
 
-becomesOccupiedSight :: WaitingAreaState -> Position -> Bool
-becomesOccupiedSight waitingAreaState@(WaitingAreaState{occupiedSeats}) (x, y) = case (x, y) `S.member` occupiedSeats of
-    False -> occupiedNeighboursCount == 0
-    True -> occupiedNeighboursCount < 5
-    where occupiedNeighboursCount = length $ filter (`S.member` occupiedSeats) neighbouringSeats
-          neighbouringSeats = neighboursBySight waitingAreaState (x, y)
-        --   allOcupiedSeats = occupiedSeats waitingAreaState
-
-
-neighboursBySight waitingAreaState candidateSeat = catMaybes $ map findFirstSeat seatSightLines
-    where findFirstSeat line = find (`S.member` allSeats) line
+neighboursBySight waitingAreaState@(WaitingAreaState{seats}) candidateSeat = catMaybes $ map findFirstSeat seatSightLines
+    where findFirstSeat line = find (`M.member` seats) line
           seatSightLines = centerSightLines waitingAreaState candidateSeat
-          allSeats = calculateSeats waitingAreaState
 
 centerSightLines waitingAreaState candidateSeat = map (boundToGrid waitingAreaState . centerLine candidateSeat) neighboursSightLines
 
@@ -96,29 +72,28 @@ neighboursSightLines = [
     ]
 
 tickSeatsAdjacent :: WaitingAreaState -> WaitingAreaState
-tickSeatsAdjacent currentState = currentState{occupiedSeats=newOccupiedSeats}
-    where newOccupiedSeats = S.filter (becomesOccupiedAdjacent currentState) seats
-          seats = calculateSeats currentState
-
-becomesOccupiedAdjacent :: WaitingAreaState -> Position -> Bool
-becomesOccupiedAdjacent WaitingAreaState{occupiedSeats} (x, y) = case (x, y) `S.member` occupiedSeats of
-    False -> occupiedNeighboursCount == 0
-    True -> occupiedNeighboursCount < 4
-    where occupiedNeighboursCount = length $ filter (`S.member` occupiedSeats) neighbouringSeats
-          neighbouringSeats = [(x+a, y+b) | b <- [-1,0,1], a <- [-1,0,1], a /= 0 || b /= 0]
-
-calculateSeats :: WaitingAreaState -> S.Set Position
-calculateSeats WaitingAreaState{gridLimit, floorPositions} = 
-    S.fromList [(x, y) | y <- [0..maxY], x <- [0..maxX], S.notMember (x, y) floorPositions]
-        where (maxX, maxY) = gridLimit
+tickSeatsAdjacent currentState@(WaitingAreaState{seats}) = currentState{seats=newSeatStates}
+    where newSeatStates = M.mapWithKey updateSeat seats
+          updateSeat (x, y) occupied = case occupied of
+                False -> occupiedNeighboursCount == 0
+                True -> occupiedNeighboursCount < 4
+            where occupiedNeighboursCount = length neighbouringSeats
+                  neighbouringSeats = [
+                      neighbourOccupied | b <- [-1,0,1], a <- [-1,0,1], 
+                        let neighbourOccupied = case M.lookup (x+a, y+b) seats of
+                                Nothing -> False
+                                (Just o) -> o,
+                        neighbourOccupied,
+                        a /= 0 || b /= 0
+                    ]
 
 -- loading map
 
 parseWaitingArea :: [[Char]] -> WaitingAreaState
-parseWaitingArea raw_input = WaitingAreaState {gridLimit = (maxX, maxY), floorPositions, occupiedSeats}
+parseWaitingArea raw_input = WaitingAreaState {gridLimit = (maxX, maxY), seats = M.union freeSeats occupiedSeats}
     where (maxX, maxY) = ((length . head) raw_input - 1 , (length raw_input) - 1)
-          floorPositions = S.fromList $ positionsWith '.' raw_input
-          occupiedSeats = S.fromList $ positionsWith '#' raw_input
+          freeSeats = M.fromList $ zip (positionsWith 'L' raw_input) (repeat False)
+          occupiedSeats = M.fromList $ zip (positionsWith '#' raw_input) (repeat True)
 
 positionsWith :: Char -> [[Char]] -> [(Int, Int)]
 positionsWith char raw_input = concat [[(x, y) | (c, x) <- (zip row [0..]), c == char] | (row, y) <- (zip raw_input [0..])]
