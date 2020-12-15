76d95b19407848f31fe9f6b5dbd7a8cab7ecc6e2
Ricardo Amendoeira <ricardo.filipe.amendoeira+github@gmail.com>
2020-12-15 12:44:10 +0000

    solution for day 15, cleaned up

--- 2020/day15.hs
+++ 2020/day15.hs
@@ -1,13 +1,9 @@
-import Text.ParserCombinators.Parsec
-
 import qualified Data.Map as M
 
 main = do
-    rawInput <- readFile "input/day15.txt"
-    let (Right startingNumbers) = parse parseNumbers "" rawInput
+    let startingNumbers = [0,8,15,2,12,1,4]
     
-    let memory = M.fromList $ zip (reverse $ tail $ reverse startingNumbers) [1..]
-    let startingIndex = (head $ reverse startingNumbers, length startingNumbers, memory)
+    let startingIndex = (last startingNumbers, length startingNumbers, M.fromList $ zip (init startingNumbers) [1..])
 
     print $ getNumberAtRound 2020 startingIndex -- 289
     print $ getNumberAtRound 30000000 startingIndex -- 1505722
@@ -15,17 +11,10 @@ main = do
 getNumberAtRound :: Int -> (Int, Int, M.Map Int Int) -> Int
 getNumberAtRound round (lastNumber, lastRound, memory)
     | round == lastRound = lastNumber
-    | otherwise = getNumberAtRound round (sayNextNumberFast (lastNumber, lastRound, memory))
+    | otherwise = getNumberAtRound round (sayNextNumber (lastNumber, lastRound, memory))
 
-sayNextNumberFast :: (Int, Int, M.Map Int Int) -> (Int, Int, M.Map Int Int)
-sayNextNumberFast (lastNumber, lastRound, memory) = case M.lookup lastNumber memory of
+sayNextNumber :: (Int, Int, M.Map Int Int) -> (Int, Int, M.Map Int Int)
+sayNextNumber (lastNumber, lastRound, memory) = case M.lookup lastNumber memory of
     Nothing -> (0, currentRound, M.insert lastNumber lastRound memory)
     (Just lastSpokenOnRound) -> (lastRound - lastSpokenOnRound, currentRound, M.insert lastNumber lastRound memory)
     where currentRound = lastRound + 1
-
-parseNumbers :: Parser [Int]
-parseNumbers = do
-    x <- read <$> many1 digit
-    optional (char ',')
-    xs <- option [] parseNumbers
-    return (x:xs)
