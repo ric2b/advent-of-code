c933c1f2aeaeb5c8e556ba8889a56a852531dbd3
Ricardo Amendoeira <ricardo.filipe.amendoeira+github@gmail.com>
2020-12-16 14:20:04 +0000

    solution for day 16 part 1 and attempt at 2, wip

---
+++ 2020/day16.hs
@@ -0,0 +1,90 @@
+import Text.ParserCombinators.Parsec
+import Data.Either (rights)
+import Data.List (concat, find, delete, isPrefixOf, elemIndex)
+import Data.Maybe (fromJust)
+
+import Debug.Trace
+
+main = do
+    raw_input <- lines <$> readFile "input/day16.txt"
+    -- raw_input <- lines <$> readFile "input/day16.example.txt"
+    -- raw_input <- lines <$> readFile "input/day16.example2.txt"
+    let raw_field_rules:(_:raw_my_ticket:[]):(_:raw_nearby_tickets):[] = splitOn "" raw_input
+    
+    let field_rules = rights $ map (parse parseFieldRule "") raw_field_rules
+    let my_ticket = map read $ splitOn ',' raw_my_ticket :: [Int]
+    let nearby_tickets = map (map read . splitOn ',') raw_nearby_tickets :: [[Int]]
+    
+    -- print my_ticket
+    -- print nearby_tickets
+
+    -- print $ parse parseFieldRule "" "departure location: 36-269 or 275-973"
+    
+    print $ sum $ filter (obviouslyInvalidValue field_rules) (concat nearby_tickets) -- 22073
+    
+    let validTickets = filter (not . (obviouslyInvalidTicket field_rules)) nearby_tickets
+
+    -- let prevRuleNames = ["arrival station", "arrival track","duration","price","route","row","seat","wagon","zone"]
+    let prevRuleNames = ["arrival track","duration","price","route","row","seat","wagon","zone"]
+    let remainingRules = filter (\FieldRule{fieldName=name} -> name `elem` prevRuleNames) field_rules
+    -- print "done"
+
+    let fieldOrder = detectFieldLocations field_rules (validTickets)
+    print fieldOrder
+    
+    let departureFieldRules = filter (\FieldRule{fieldName=name} -> "departure" `isPrefixOf` name) field_rules
+    print departureFieldRules
+
+    let departureFieldIndexes = map (fromJust . (`elemIndex` fieldOrder)) departureFieldRules
+    print $ product $ map (my_ticket !!) departureFieldIndexes
+
+    -- Too low: 920453493481
+
+    -- Arrival platform could be index 12???
+    -- Type could be index 12???
+
+    
+
+
+data FieldRule = FieldRule { fieldName::String, rangeA::(Int, Int), rangeB::(Int, Int) } deriving (Show, Eq)
+
+-- detectFieldLocations :: [FieldRule] -> [[Int]] -> [FieldRule]
+detectFieldLocations :: [FieldRule] -> [[Int]] -> [FieldRule]
+detectFieldLocations fieldRules tickets | trace (show (length fieldRules)) False = undefined
+-- detectFieldLocations fieldRules tickets | trace (show 1) False = undefined
+
+detectFieldLocations [] tickets = []
+detectFieldLocations fieldRules tickets = ruleAtCurrentIndex:(detectFieldLocations (delete ruleAtCurrentIndex fieldRules) tickets)
+    where ruleAtCurrentIndex = fromJust $ find allValuesAtIndexValidForRule fieldRules
+          allValuesAtIndexValidForRule fieldRule = all (valueInsideBounds fieldRule) valuesAtIndex
+          valuesAtIndex = map (!! currentIndex) tickets
+          currentIndex = totalFields - length fieldRules
+          totalFields = length (head tickets)
+
+obviouslyInvalidTicket :: [FieldRule] -> [Int] -> Bool
+obviouslyInvalidTicket fieldRules ticketValues = any (obviouslyInvalidValue fieldRules) ticketValues
+
+obviouslyInvalidValue :: [FieldRule] -> Int -> Bool
+obviouslyInvalidValue fieldRules fieldValue = all (not . ((flip valueInsideBounds) fieldValue)) fieldRules
+
+valueInsideBounds :: FieldRule -> Int -> Bool
+valueInsideBounds FieldRule{rangeA=(rangeAMin, rangeAMax), rangeB=(rangeBMin, rangeBMax)} fieldValue = 
+    (rangeAMin <= fieldValue && fieldValue <= rangeAMax) 
+    || (rangeBMin <= fieldValue && fieldValue <= rangeBMax)
+
+parseFieldRule :: Parser FieldRule
+parseFieldRule = do
+        fieldName <- many1 (noneOf ":")
+        string ": "
+        rangeAMin <- read <$> many1 digit
+        char '-'
+        rangeAMax <- read <$> many1 digit
+        string " or "
+        rangeBMin <- read <$> many1 digit
+        char '-'
+        rangeBMax <- read <$> many1 digit
+        return (FieldRule {fieldName = fieldName, rangeA = (rangeAMin, rangeAMax) , rangeB = (rangeBMin, rangeBMax)})
+
+splitOn delimiter xs = case break (== delimiter) xs of
+    (y, _ : ys) -> y : splitOn delimiter ys
+    (y, []) -> [y]
