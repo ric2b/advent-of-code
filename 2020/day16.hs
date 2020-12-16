90de82123e3040614a7c2cc52e23e8e51535e5f6
Ricardo Amendoeira <ricardo.filipe.amendoeira+github@gmail.com>
2020-12-16 20:06:43 +0000

    solution for day 16 part 2, ugly

--- 2020/day16.hs
+++ 2020/day16.hs
@@ -1,6 +1,6 @@
 import Text.ParserCombinators.Parsec
 import Data.Either (rights)
-import Data.List (concat, find, delete, isPrefixOf, elemIndex)
+import Data.List (concat, find, findIndex, delete, isPrefixOf, elemIndex)
 import Data.Maybe (fromJust)
 
 import Debug.Trace
@@ -24,42 +24,49 @@ main = do
     
     let validTickets = filter (not . (obviouslyInvalidTicket field_rules)) nearby_tickets
 
+    print $ map (length . (validRulesAtIndex field_rules validTickets)) [0..(length field_rules) -1]
+
     -- let prevRuleNames = ["arrival station", "arrival track","duration","price","route","row","seat","wagon","zone"]
     let prevRuleNames = ["arrival track","duration","price","route","row","seat","wagon","zone"]
     let remainingRules = filter (\FieldRule{fieldName=name} -> name `elem` prevRuleNames) field_rules
-    -- print "done"
 
-    let fieldOrder = detectFieldLocations field_rules (validTickets)
-    print fieldOrder
-    
-    let departureFieldRules = filter (\FieldRule{fieldName=name} -> "departure" `isPrefixOf` name) field_rules
-    print departureFieldRules
+    print $ map (validRulesAtIndex field_rules validTickets) [0..(length (head validTickets)) -1]
 
-    let departureFieldIndexes = map (fromJust . (`elemIndex` fieldOrder)) departureFieldRules
-    print $ product $ map (my_ticket !!) departureFieldIndexes
+    let fieldOrder = detectFieldLocations field_rules validTickets
+    let departureFieldIndexes = map fst $ filter (\(i, FieldRule{fieldName=name}) -> "departure" `isPrefixOf` name) fieldOrder
 
-    -- Too low: 920453493481
+    print $ product $ map (my_ticket !!) departureFieldIndexes -- 1346570764607
 
-    -- Arrival platform could be index 12???
-    -- Type could be index 12???
-
+    print "done"
     
 
 
 data FieldRule = FieldRule { fieldName::String, rangeA::(Int, Int), rangeB::(Int, Int) } deriving (Show, Eq)
 
 -- detectFieldLocations :: [FieldRule] -> [[Int]] -> [FieldRule]
-detectFieldLocations :: [FieldRule] -> [[Int]] -> [FieldRule]
-detectFieldLocations fieldRules tickets | trace (show (length fieldRules)) False = undefined
+-- detectFieldLocations :: [FieldRule] -> [[Int]] -> [FieldRule]
+-- detectFieldLocations fieldRules tickets | trace (show (length fieldRules)) False = undefined
 -- detectFieldLocations fieldRules tickets | trace (show 1) False = undefined
 
+-- detectFieldLocations [] tickets = []
+-- detectFieldLocations fieldRules tickets = ruleAtCurrentIndex:(detectFieldLocations (delete ruleAtCurrentIndex fieldRules) tickets)
+--     where ruleAtCurrentIndex = fromJust $ find allValuesAtIndexValidForRule fieldRules
+--           allValuesAtIndexValidForRule fieldRule = all (valueInsideBounds fieldRule) valuesAtIndex
+--           valuesAtIndex = map (!! currentIndex) tickets
+--           currentIndex = totalFields - length fieldRules
+--           totalFields = length (head tickets)
+
+detectFieldLocations :: [FieldRule] -> [[Int]] -> [(Int, FieldRule)]
 detectFieldLocations [] tickets = []
-detectFieldLocations fieldRules tickets = ruleAtCurrentIndex:(detectFieldLocations (delete ruleAtCurrentIndex fieldRules) tickets)
-    where ruleAtCurrentIndex = fromJust $ find allValuesAtIndexValidForRule fieldRules
-          allValuesAtIndexValidForRule fieldRule = all (valueInsideBounds fieldRule) valuesAtIndex
-          valuesAtIndex = map (!! currentIndex) tickets
-          currentIndex = totalFields - length fieldRules
+detectFieldLocations fieldRules tickets = (nextFieldIndex, nextRule):(detectFieldLocations (delete nextRule fieldRules) tickets)
+    where nextRule = head $ validRulesAtIndexes !! nextFieldIndex
+          nextFieldIndex = fromJust $ findIndex ((==1) . length) validRulesAtIndexes
+          validRulesAtIndexes = map (validRulesAtIndex fieldRules tickets) [0..totalFields-1]
           totalFields = length (head tickets)
+
+validRulesAtIndex fieldRules tickets i = filter allValuesAtIndexValidForRule fieldRules
+    where allValuesAtIndexValidForRule fieldRule = all (valueInsideBounds fieldRule) valuesAtIndex
+          valuesAtIndex = map (!! i) tickets
 
 obviouslyInvalidTicket :: [FieldRule] -> [Int] -> Bool
 obviouslyInvalidTicket fieldRules ticketValues = any (obviouslyInvalidValue fieldRules) ticketValues
