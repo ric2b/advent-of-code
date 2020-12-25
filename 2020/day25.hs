de74153eb3880185a1215609df72861ce6f5c557
Ricardo Amendoeira <ricardo.filipe.amendoeira+github@gmail.com>
2020-12-25 12:15:23 +0000

    solution for day 25, cleaned up further

--- 2020/day25.hs
+++ 2020/day25.hs
@@ -1,9 +1,8 @@
 main = do
     (publicKeyA:publicKeyB:[]) <- map read <$> lines <$> readFile "input/day25.txt"
-    print $ transform publicKeyB (breakLoopNumber publicKeyA (1, 0)) -- 6011069
+    print $ transform publicKeyB (breakLoopNumber publicKeyA) -- 6011069
 
-breakLoopNumber publicKey (value, loop) = 
-    if publicKey == value then loop else breakLoopNumber publicKey (step 7 value, loop+1)
+breakLoopNumber publicKey = snd $ until ((== publicKey) . fst) (\(value, i) -> (step 7 value, i+1)) (1, 0)
 
 transform subjectNumber loops = iterate (step subjectNumber) 1 !! loops
 
