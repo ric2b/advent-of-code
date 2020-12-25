e67855fe0d898b445a1f1b961de2011aef33144c
Ricardo Amendoeira <ricardo.filipe.amendoeira+github@gmail.com>
2020-12-25 11:45:42 +0000

    solution for day 25, cleaned up

--- 2020/day25.hs
+++ 2020/day25.hs
@@ -1,50 +1,10 @@
-import Data.List (find)
-import Data.Maybe (fromJust)
-
 main = do
     (publicKeyA:publicKeyB:[]) <- map read <$> lines <$> readFile "input/day25.txt"
-    -- (publicKeyA:publicKeyB:[]) <- map read <$> lines <$> readFile "input/day25.example.txt"
-    -- print publicKeyA
-    -- print publicKeyB
-    -- print $ publicKeyA + publicKeyB
-    -- print $ transform 7 8
-    -- let loopSizeA = breakLoopNumber publicKeyA
-    -- let loopSizeB = breakLoopNumber publicKeyB
-    -- let encryptionKeyA = transform publicKeyA loopSizeB
-    -- let encryptionKeyB = transform publicKeyB loopSizeA
-    -- print encryptionKeyA
-    -- print encryptionKeyB
-    -- print $ memoizedTransfrom 7 10000000
-    -- print $ memoizedTransfrom 7 10000001
-    -- print $ memoizedTransfrom 7 10000002
-    -- print $ memoizedTransfrom 7 10000003
-    -- print $ memoizedTransfrom 7 10000004
+    print $ transform publicKeyB (breakLoopNumber publicKeyA (1, 0)) -- 6011069
 
-    let loopSizeA = fastBreakLoopNumber publicKeyA (1, 0)
-    let loopSizeB = fastBreakLoopNumber publicKeyB (1, 0)
-    let encryptionKeyA = transform publicKeyA loopSizeB
-    let encryptionKeyB = transform publicKeyB loopSizeA
-    print encryptionKeyA -- 6011069
-    print encryptionKeyB -- 6011069
-    -- print $ breakLoopNumber publicKeyA
+breakLoopNumber publicKey (value, loop) = 
+    if publicKey == value then loop else breakLoopNumber publicKey (step 7 value, loop+1)
 
--- diffieHellman :: 
--- diffieHellman
+transform subjectNumber loops = iterate (step subjectNumber) 1 !! loops
 
-breakLoopNumber :: Int -> Int
-breakLoopNumber publicKey = fromJust $ find ((== publicKey) . (transform 7)) [1..]
-
--- memoized_fib :: Int -> Integer
--- memoized_fib = (map fib [0 ..] !!)
---    where fib 0 = 0
---          fib 1 = 1
---          fib n = memoized_fib (n-2) + memoized_fib (n-1)
-
-fastBreakLoopNumber :: Int -> (Int, Int) -> Int
-fastBreakLoopNumber publicKey (currentValue, loopNumber)
-    | publicKey == currentValue = loopNumber
-    | otherwise = fastBreakLoopNumber publicKey (currentValue * 7 `mod` 20201227, loopNumber + 1)
-
-transform :: Int -> Int -> Int
--- transform subjectNumber = (iterate (\value -> value * subjectNumber `mod` 20201227) 1 !!)
-transform subjectNumber loops = iterate (\value -> value * subjectNumber `mod` 20201227) 1 !! loops
+step subjectNumber value = value * subjectNumber `mod` 20201227
