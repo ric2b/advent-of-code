a86ad75c70de1715ac8a688c952bcd0a5178a534
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2021-12-06 15:37:26 +0000

    Add day 3

---
+++ 2021/src/routes/days/03.svelte
@@ -0,0 +1,51 @@
+<script>
+	import { onMount } from 'svelte';
+    import { assets } from '$app/paths';
+
+	let part1_result, part2_result;
+	onMount(async () => {
+		const raw_input = await fetch(`${assets}/inputs/day03.txt`).then(r => r.text());
+        const input = parse(raw_input);
+		part1_result = part1(input);
+        part2_result = part2(input);
+    });
+
+    function parse(raw_input) {
+        return raw_input.split('\n').filter(s => s !== '').map(m => m.split('').map(d => Number(d)));
+    }
+
+    function part1(diagnostics) {
+        const gamma_bits = common_bits(diagnostics);
+        const epsilon_bits = gamma_bits.map(bit => bit === 0 ? 1 : 0);
+
+        const gamma = parseInt(gamma_bits.join(''), 2);
+        const epsilon = parseInt(epsilon_bits.join(''), 2);
+
+        return gamma * epsilon
+    }
+
+    function part2(diagnostics) {
+        let o2_candidates = diagnostics, co2_candidates = diagnostics;
+
+        for (let i of Array(diagnostics[0].length).keys()) {
+            o2_candidates = o2_candidates.filter(diagnostic => diagnostic[i] === common_bits(o2_candidates)[i])
+            if (co2_candidates.length > 1) {
+                co2_candidates = co2_candidates.filter(diagnostic => diagnostic[i] !== common_bits(co2_candidates)[i])
+            }
+        }
+
+        const o2_generator_rating = parseInt(o2_candidates[0].join(''), 2);
+        const co2_scrubber_rating = parseInt(co2_candidates[0].join(''), 2);
+
+        return o2_generator_rating * co2_scrubber_rating;
+    }
+
+    function common_bits(diagnostics) {
+        const one_counts = Array(diagnostics[0].length).fill(0);
+        diagnostics.forEach(diagnostic => diagnostic.forEach((value, i) => one_counts[i] += value));
+        return one_counts.map(one_count => one_count >= diagnostics.length / 2 ? 1 : 0);
+    }
+</script>
+
+<p>Part 1: {part1_result}</p>
+<p>Part 2: {part2_result}</p>
--- 2021/src/routes/index.svelte
+++ 2021/src/routes/index.svelte
@@ -1,5 +1,13 @@
 <h1>Advent of Code 2021</h1>
+<!-- TODO:
+    - Make list of pages dynamic
+    - More code sharing between pages
+    - Support changing inputs
+    - Viz for day 2
+    - Christmas theme
+-->
 <ul>
     <li><a href="days/01">Day 1</a></li>
     <li><a href="days/02">Day 2</a></li>
+    <li><a href="days/03">Day 3</a></li>
 </ul>
