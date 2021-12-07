61ef101a47368ac82a7b361e13873dd6419ab5c2
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2021-12-07 20:52:50 +0000

    Add day 7 (part 2)

--- 2021/src/routes/days/07.svelte
+++ 2021/src/routes/days/07.svelte
@@ -17,6 +17,16 @@
     function part1(positions) {
         const fuel_required = (positions, new_x) => positions.map(x => Math.abs(x - new_x)).reduce((a,b) => a + b);
 
+        const median_position = positions.sort((a,b) => a-b)[Math.floor(positions.length / 2)];
+        return fuel_required(positions, median_position);
+    }
+
+    function part2(positions) {
+        const fuel_required = (positions, new_x) => positions.map(x => {
+            const distance = Math.abs(x - new_x);
+            return distance*(distance+1) / 2
+        }).reduce((a, b) => a + b);
+
         let optimal_fuel_required = Infinity;
         for (let i = Math.min(...positions); i <= Math.max(...positions); i++) {
             const current_fuel_required = fuel_required(positions, i);
@@ -25,10 +35,6 @@
             }
         }
         return optimal_fuel_required;
-    }
-
-    function part2(input) {
-        return input;
     }
 </script>
 
