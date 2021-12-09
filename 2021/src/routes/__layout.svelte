6f97f0b06c6a0c1c1e2f67fe4459f993d143d20a
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2021-12-09 18:48:17 +0000

    Update index

---
+++ 2021/src/routes/__layout.svelte
@@ -0,0 +1,7 @@
+<header>
+    <h1><a href="/">Advent of Code 2021</a></h1>
+
+    <a href="https://github.com/ric2b/advent-of-code/tree/master/2021">[Source Code]</a>
+</header>
+
+<slot></slot>
--- 2021/src/routes/index.svelte
+++ 2021/src/routes/index.svelte
@@ -1,19 +1,18 @@
-<h1>Advent of Code 2021</h1>
 <!-- TODO:
     - Make list of pages dynamic
     - More code sharing between pages
     - Support changing inputs
-    - Viz for days: 2, 4, 5
+    - Viz for days: 2, 4, 5, 8, 9
     - Christmas theme
 -->
 <ul>
-    <li><a href="days/01">Day 1</a></li>
-    <li><a href="days/02">Day 2</a></li>
-    <li><a href="days/03">Day 3</a></li>
-    <li><a href="days/04">Day 4</a></li>
-    <li><a href="days/05">Day 5</a></li>
-    <li><a href="days/06">Day 6</a></li>
-    <li><a href="days/07">Day 7</a></li>
-    <li><a href="days/08">Day 8</a></li>
-    <li><a href="days/09">Day 9</a></li>
+    <li><a href="days/01">Day 1: Sonar Sweep</a></li>
+    <li><a href="days/02">Day 2: Dive!</a></li>
+    <li><a href="days/03">Day 3: Binary Diagnostic</a></li>
+    <li><a href="days/04">Day 4: Giant Squid</a></li>
+    <li><a href="days/05">Day 5: Hydrothermal Venture</a></li>
+    <li><a href="days/06">Day 6: Lanternfish</a></li>
+    <li><a href="days/07">Day 7: The Treachery of Whales </a></li>
+    <li><a href="days/08">Day 8: Seven Segment Search</a></li>
+    <li><a href="days/09">Day 9: Smoke Basin</a></li>
 </ul>
