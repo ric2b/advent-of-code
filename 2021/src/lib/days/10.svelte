1ac1c5dc55fddec317f97f320f3841bb62ec0cc8
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2021-12-10 15:39:46 +0000

    Add Day 10 (part 1)

--- 2021/src/lib/days/10.svelte
+++ 2021/src/lib/days/10.svelte
@@ -1,8 +1,23 @@
 <script>
-    const parse = raw_input => raw_input.split('\n').filter(l => l !== '');
+    const parse = raw_input => raw_input.split('\n').filter(l => l !== '').map(l => l.split(''));
 
     function part1(input) {
-        return JSON.stringify(input);
+        const chunk_chars = { ')': '(', ']': '[', '}': '{', '>': '<'};
+        const error_points = { ')': 3, ']': 57, '}': 1197, '>': 25137 }
+        const score = subsystem => {
+            const chunk_queue = [];
+            for (const c of subsystem) {
+                if (Object.values(chunk_chars).includes(c)) {
+                    chunk_queue.push(c);
+                } else if (Object.keys(chunk_chars).includes(c)) {
+                    if (chunk_queue.pop() !== chunk_chars[c]) return error_points[c];
+                }
+            }
+        };
+
+        const error_scores = input.map(score).filter(Boolean);
+
+        return error_scores.reduce((a, b) => a + b);
     }
 
     function part2(input) {
