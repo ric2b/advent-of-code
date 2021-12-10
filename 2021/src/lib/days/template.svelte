cb031fc36f504b8a4cf766bcce75db38b4e3decb
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2021-12-10 16:34:05 +0000

    Fix indent

--- 2021/src/lib/days/template.svelte
+++ 2021/src/lib/days/template.svelte
@@ -14,6 +14,6 @@
     export let part2_result;
 
     $: input = parse(raw_input);
-	$: part1_result = part1(input);
+    $: part1_result = part1(input);
     $: part2_result = part2(input);
 </script>
