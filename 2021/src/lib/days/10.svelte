8b80fb55badf62da4a3c4581c977ac63cbe13164
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2021-12-10 15:54:43 +0000

    Add Day 10 (part 2)

--- 2021/src/lib/days/10.svelte
+++ 2021/src/lib/days/10.svelte
@@ -1,27 +1,41 @@
 <script>
+    const chunk_chars = { ')': '(', ']': '[', '}': '{', '>': '<'};
+
     const parse = raw_input => raw_input.split('\n').filter(l => l !== '').map(l => l.split(''));
 
     function part1(input) {
-        const chunk_chars = { ')': '(', ']': '[', '}': '{', '>': '<'};
-        const error_points = { ')': 3, ']': 57, '}': 1197, '>': 25137 }
-        const score = subsystem => {
-            const chunk_queue = [];
+        return input.map(score_subsystem).reduce((a, b) => a + b);
+    }
+
+    function part2(input) {
+        const completion_points = { '(': 1, '[': 2, '{': 3, '<': 4 }
+
+        const scores = input.filter(score => score_subsystem(score) === 0).map(subsystem => {
+            const chunk_queue = []
             for (const c of subsystem) {
                 if (Object.values(chunk_chars).includes(c)) {
                     chunk_queue.push(c);
                 } else if (Object.keys(chunk_chars).includes(c)) {
-                    if (chunk_queue.pop() !== chunk_chars[c]) return error_points[c];
+                    chunk_queue.pop()
                 }
             }
-        };
+            return chunk_queue.reverse().reduce((score, c) => score * 5 + completion_points[c], 0);
+        });
 
-        const error_scores = input.map(score).filter(Boolean);
-
-        return error_scores.reduce((a, b) => a + b);
+        return scores.sort((a, b) => a - b)[Math.floor(scores.length / 2)];
     }
 
-    function part2(input) {
-        return JSON.stringify(input);
+    function score_subsystem(subsystem) {
+        const chunk_queue = [], error_points = { ')': 3, ']': 57, '}': 1197, '>': 25137 }
+
+        for (const c of subsystem) {
+            if (Object.values(chunk_chars).includes(c)) {
+                chunk_queue.push(c);
+            } else if (Object.keys(chunk_chars).includes(c)) {
+                if (chunk_queue.pop() !== chunk_chars[c]) return error_points[c];
+            }
+        }
+        return 0;
     }
 
     export let raw_input;
