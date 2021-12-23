922f87126d44c35d0404a7b98f7ba315aaa35fe4
Ricardo Amendoeira <ricardo.filipe.amendoeira+github@gmail.com>
2021-12-23 21:36:20 +0000

    Fix GH link on index

--- 2021/src/routes/__layout.svelte
+++ 2021/src/routes/__layout.svelte
@@ -10,7 +10,7 @@
     import { page } from '$app/stores';
     const repo_link = 'https://github.com/ric2b/advent-of-code';
     $: day = Number($page.path.split('/').slice(-1)[0]);
-    $: source_link = !isNaN(day) ?
+    $: source_link = day > 0 ?
         `${repo_link}/blob/master/2021/src/routes/day/${day}.svelte`
         : `${repo_link}/tree/master/2021`;
 </script>
