8c15b49952d3c2e193fdb297bd7d93fb46c32b80
Ricardo Amendoeira <ricardo.filipe.amendoeira+github@gmail.com>
2021-12-23 21:08:07 +0000

    Refactor day 1

---
+++ 2021/src/routes/day/01.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+    export async function load({ stuff }) { return { props: stuff }; }
+</script>
+
+<script>
+    const parse = raw_input => raw_input.split('\n').map(s => Number(s));
+
+    const part1 = depths => depths.filter((value, i) => depths[i+1] > value).length;
+    const part2 = depths => depths.filter((value, i) => window(depths, i + 1) > window(depths, i)).length;
+
+    const window = (depths, i) => depths.slice(i, i + 3).reduce((a, b) => a + b, 0);
+
+    export let raw_input;
+    export let set_part1_result;
+    export let set_part2_result;
+
+    $: input = parse($raw_input);
+    $: set_part1_result(part1(input));
+    $: set_part2_result(part2(input));
+</script>
---
+++ 2021/src/routes/day/__layout.svelte
@@ -0,0 +1,49 @@
+<script context="module">
+    import { assets } from '$app/paths';
+    import { writable } from "svelte/store";
+    export async function load({ page, fetch }) {
+        const day = page.path.split('/').slice(-1)[0];
+        const metadata_url = `${assets}/metadata.json`;
+        const input_url = `${assets}/inputs/day${day}.txt`;
+
+        const responses = await Promise.all([fetch(metadata_url), fetch(input_url)]);
+        for (const res of responses) {
+            if (!res.ok) return { status: res.status, error: new Error(`Could not load ${input_url}`) };
+        }
+
+        const [metadata_res, input_res] = responses;
+        const metadata = await metadata_res.json(), initial_raw_input = await input_res.text();
+
+        const raw_input = writable(initial_raw_input);
+        const { set: set_part1_result, ...part1_result } = writable('...');
+        const { set: set_part2_result, ...part2_result } = writable('...');
+
+        return {
+            props: { raw_input, part1_result, part2_result, metadata },
+            stuff: { set_part1_result, set_part2_result, raw_input }
+        };
+    }
+</script>
+
+<script>
+    import { page } from '$app/stores';
+    const day = Number($page.path.split('/').slice(-1)[0]);
+    export let metadata;
+    export let raw_input;
+    export let part1_result;
+    export let part2_result;
+</script>
+
+<article>
+    <h2>--- {metadata.titles[day - 1]} ---</h2>
+
+    <p class:aoc_yellow={$part1_result !== '...'}>Part 1: {$part1_result}</p>
+    <p class:aoc_yellow={$part2_result !== '...'}>Part 2: {$part2_result}</p>
+
+    <slot />
+
+    <details>
+        <summary>Change input</summary>
+        <textarea rows="20" cols="80" bind:value={$raw_input}></textarea>
+    </details>
+</article>
