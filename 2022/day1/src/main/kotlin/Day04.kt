9fd74c1c300b48f1f4ca7b579711dc7e242ed4b1
ricardoamendoeira <ricardo.filipe.amendoeira+github@gmail.com>
2022-12-04 12:12:44 +0000

    Add day 4

---
+++ 2022/day1/src/main/kotlin/Day04.kt
@@ -0,0 +1,25 @@
+package day04
+
+fun part1(input: String): Int {
+    val pairs: List<Pair<IntRange, IntRange>> = input.trimEnd().lines().map { rawPair ->
+        val (elfA, elfB) = rawPair.split(',').map { rawRange ->
+            val (a, b) = rawRange.split('-').map { it.toInt() }
+            IntRange(a, b)
+        }
+        Pair(elfA, elfB)
+    }
+
+    return pairs.count { (elfA, elfB) -> elfA.all { elfB.contains(it) } || elfB.all { elfA.contains(it) } }
+}
+
+fun part2(input: String): Int {
+    val pairs: List<Pair<IntRange, IntRange>> = input.trimEnd().lines().map { rawPair ->
+        val (elfA, elfB) = rawPair.split(',').map { rawRange ->
+            val (a, b) = rawRange.split('-').map { it.toInt() }
+            IntRange(a, b)
+        }
+        Pair(elfA, elfB)
+    }
+
+    return pairs.count { (elfA, elfB) -> elfA.any { elfB.contains(it) } || elfB.any { elfA.contains(it) } }
+}
---
+++ 2022/day1/src/test/kotlin/Day04KtTest.kt
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.io.File
+
+class Day04KtTest {
+    @Test
+    fun testPart1() {
+        val testInput =
+            """
+                2-4,6-8
+                2-3,4-5
+                5-7,7-9
+                2-8,3-7
+                6-6,4-6
+                2-6,4-8
+            """.trimIndent()
+
+        assertEquals(2, day04.part1(testInput))
+    }
+
+    @Test
+    fun testPart1MyInput() {
+        val myInput = File("src/main/resources/inputs/day04.txt").readText()
+        assertEquals(644, day04.part1(myInput))
+    }
+
+    @Test
+    fun testPart2MyInput() {
+        val myInput = File("src/main/resources/inputs/day04.txt").readText()
+        assertEquals(926, day04.part2(myInput))
+    }
+}
