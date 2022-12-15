2c7ba04f3cf047d64ebb518369d8613ebde3b30f
ricardoamendoeira <ricardo.filipe.amendoeira+github@gmail.com>
2022-12-15 11:47:00 +0000

    Make day 15 pt 1 faster

--- 2022/project/src/main/kotlin/Day15.kt
+++ 2022/project/src/main/kotlin/Day15.kt
@@ -2,6 +2,7 @@ package day15
 
 import kotlin.math.abs
 import kotlin.math.max
+import kotlin.math.min
 
 data class Pos(val x: Int, val y: Int)
 data class Region(val center: Pos, val distance: Int) {
@@ -11,17 +12,12 @@ data class Region(val center: Pos, val distance: Int) {
         return dx + dy <= distance
     }
 
-    fun pointsAtY(y: Int): Set<Pos> {
-        val xRange = distance - abs(y - center.y)
-        return (-xRange..xRange)
-            .map { dx -> Pos(center.x + dx, y) }
-            .toSet()
-    }
-
     fun rangeAtY(y: Int): IntRange {
         val xRange = distance - abs(y - center.y)
         return if (xRange > 0) (center.x - xRange)..(center.x + xRange) else IntRange.EMPTY
     }
+
+    fun pointsAtY(y: Int): Set<Pos> = rangeAtY(y).map { Pos(it, y) }.toSet()
 }
 
 data class Sensor(val position: Pos, val closestBeacon: Pos) {
@@ -40,21 +36,29 @@ fun part1(input: String, y: Int = 2000000): Int {
         Sensor(Pos(sx, sy), Pos(bx, by))
     }.toSet()
 
+    // Simpler approach
+    //    val rangesAtY = sensors.map { it.emptyRegion().rangeAtY(y) }
+    //    val rangeAtY = (rangesAtY.minOf { it.start }..rangesAtY.maxOf { it.endInclusive })
+    //    val beaconsAtY = sensors.map { it.closestBeacon }.filter { it.y == y }.toSet().size
+    //
+    //    return rangeAtY.count { x -> rangesAtY.any { it.contains(x) } } - beaconsAtY
+
+    // Faster approach
     val sortedRangesAtY = sensors.map { it.emptyRegion().rangeAtY(y) }.filter { !it.isEmpty() }.sortedBy { it.start }
     val reducedRangesAtY = mutableListOf(sortedRangesAtY.first())
 
     sortedRangesAtY.drop(1).forEach {
-        val last = reducedRangesAtY.removeLast()
+        val last = reducedRangesAtY.last()
+
         if (it.start <= last.endInclusive) {
+            reducedRangesAtY.removeLast()
             reducedRangesAtY.add(last.start..max(it.endInclusive, last.endInclusive))
         } else {
-            reducedRangesAtY.add(last)
             reducedRangesAtY.add(it)
         }
     }
 
     return reducedRangesAtY.sumOf { it.endInclusive - it.start }
-//    return sensors.flatMap { it.emptyRegion().pointsAtY(y).minus(it.closestBeacon) }.toSet().size
 }
 
 fun part2(input: String, range: IntRange = 0..4000000): Long {
@@ -65,15 +69,15 @@ fun part2(input: String, range: IntRange = 0..4000000): Long {
 
     val emptyRegions = sensors.map { it.emptyRegion() }
 
-    emptyRegions.forEach {
-        (-it.distance+1..it.distance+1).forEach { dx ->
-            listOf(
-                Pos(it.center.x + dx, it.center.y + (it.distance+1 - dx)),
-                Pos(it.center.x + dx, it.center.y - (it.distance+1 - dx))
-            )
-                .filter { neighbour -> neighbour.x in range && neighbour.y in range }
+    emptyRegions.forEach { region ->
+        val dxRange = -min(region.distance, region.center.x)..min(region.distance, range.endInclusive - region.center.x)
+        dxRange.forEach { dx ->
+            val dy = (region.distance + 1 - dx)
+
+            listOf(Pos(region.center.x + dx, region.center.y + dy), Pos(region.center.x + dx, region.center.y - dy))
+                .filter { neighbour -> neighbour.y in range }
                 .forEach { neighbour ->
-                    if (emptyRegions.none { r -> r contains neighbour }) {
+                    if (emptyRegions.none { it contains neighbour }) {
                         return 4000000 * neighbour.x.toLong() + neighbour.y.toLong()
                     }
                 }
