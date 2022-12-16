60c81ec245de74b580959802cc6f8ed1ab163b7c
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2022-12-16 13:14:29 +0000

    Add day 16

--- 2022/project/src/main/kotlin/Day16.kt
+++ 2022/project/src/main/kotlin/Day16.kt
@@ -1,7 +1,69 @@
 package day16
 
+import kotlin.math.max
+
+typealias ValveId = String
+data class Valve(val id: ValveId, val flowRate: Long, val connectedTo: List<ValveId>, var isOpen: Boolean = false)
+
+val moveToValues: MutableMap<Triple<Valve, Set<Valve>, Int>, Long> = mutableMapOf()
+
+fun moveToValue(graph: Map<ValveId, Valve>, currentValve: Valve, openValves: Set<Valve>, currentMinute: Int): Long {
+    return moveToValues.getOrPut(Triple(currentValve, openValves, currentMinute)) {
+        if (currentMinute >= 30) return 0
+
+        val pressureReleasedThisMinute = openValves.sumOf { it.flowRate }
+        val valueOfMoving = currentValve.connectedTo.maxOf { moveToValue(graph, graph.getValue(it), openValves, currentMinute + 1) }
+
+        if (currentValve in openValves || currentValve.flowRate == 0L) {
+           return@getOrPut pressureReleasedThisMinute + valueOfMoving
+        }
+
+        val valueOfOpening = moveToValue(graph, currentValve, openValves = openValves + currentValve, currentMinute + 1)
+
+        return@getOrPut pressureReleasedThisMinute + max(valueOfMoving, valueOfOpening)
+    }
+}
+
 fun part1(input: String): Long {
-    throw NotImplementedError()
+    val graph: Map<ValveId, Valve> = input.trimEnd().lines().associate { line ->
+        Regex("Valve (?<valve>\\w+) has flow rate=(\\d+); tunnel[s]? lead[s]? to valve[s]? ([\\w,\\s]+)+")
+            .findAll(line).first().let {
+                val (_, valveId, flowRate, rawConnectedValves) = it.groupValues
+                valveId to Valve(valveId, flowRate.toLong(), rawConnectedValves.split(", "))
+            }
+    }
+
+    var current = graph.getValue("AA")
+    var releasedPressure = 0L
+
+    for (minute in 1..30) {
+        println("== Minute $minute ==")
+
+        val openValves = graph.values.filter { it.isOpen }.toSet()
+        val releasedThisMinute = openValves.sumOf { it.flowRate }
+        releasedPressure += releasedThisMinute
+
+        println("Valves ${openValves.map { it.id }} are open, releasing $releasedThisMinute pressure")
+
+        if (graph.values.any { !it.isOpen && it.flowRate > 0 }) {
+            val neighbours = current.connectedTo.map { graph.getValue(it) }
+
+            val bestNeighbor = neighbours.maxByOrNull { moveToValue(graph, it, openValves, minute ) }!!
+
+            val valueOfMoving =  moveToValue(graph, bestNeighbor, openValves, minute)
+            val valueOfOpening = moveToValue(graph, current, openValves = openValves + current, minute)
+
+            if (current.isOpen || valueOfMoving > valueOfOpening) {
+                println("You move to valve ${bestNeighbor.id}.")
+                current = bestNeighbor
+            } else {
+                println("You open valve ${current.id}.")
+                current.isOpen = true
+            }
+        }
+    }
+
+    return releasedPressure
 }
 
 fun part2(input: String): Long {
--- 2022/project/src/test/kotlin/Day16KtTest.kt
+++ 2022/project/src/test/kotlin/Day16KtTest.kt
@@ -1,21 +1,36 @@
+
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class Day16KtTest {
     private val testInput =
         """
-
+            Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+            Valve BB has flow rate=13; tunnels lead to valves CC, AA
+            Valve CC has flow rate=2; tunnels lead to valves DD, BB
+            Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+            Valve EE has flow rate=3; tunnels lead to valves FF, DD
+            Valve FF has flow rate=0; tunnels lead to valves EE, GG
+            Valve GG has flow rate=0; tunnels lead to valves FF, HH
+            Valve HH has flow rate=22; tunnel leads to valve GG
+            Valve II has flow rate=0; tunnels lead to valves AA, JJ
+            Valve JJ has flow rate=21; tunnel leads to valve II
         """.trimIndent()
     val myInput = this::class.java.classLoader.getResource("day16.txt")!!.readText()
 
     @Test
-    fun `test part 1 with example`() = assertEquals(-1, day16.part1(testInput))
+    fun parser() {
+
+    }
 
     @Test
-    fun `test part 1 with my input`() = assertEquals(-1, day16.part1(myInput))
+    fun `test part 1 with example`() = assertEquals(1651, day16.part1(testInput))
 
     @Test
-    fun `test part 2 with example`() = assertEquals(-1, day16.part2(testInput))
+    fun `test part 1 with my input`() = assertEquals(1775, day16.part1(myInput))
+
+    @Test
+    fun `test part 2 with example`() = assertEquals(1707, day16.part2(testInput))
 
     @Test
     fun `test part 2 with my input`() = assertEquals(-1, day16.part2(myInput))
