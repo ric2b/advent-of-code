ed82b2d28f6002c88b83882dee71a47804c1fc7b
ricardoamendoeira <ricardo.filipe.amendoeira+github@gmail.com>
2022-12-20 07:17:23 +0000

    Add day 20

--- 2022/project/src/main/kotlin/Day20.kt
+++ 2022/project/src/main/kotlin/Day20.kt
@@ -1,9 +1,44 @@
 package day20
 
+import java.lang.Math.floorMod
+
 fun part1(input: String): Int {
-    throw NotImplementedError()
+    val encryptedMessage: List<Int> = input.trimEnd().lines().map(String::toInt)
+
+    val indexesAsId = encryptedMessage.indices.toMutableList()
+
+    encryptedMessage.forEachIndexed { id, n ->
+        val i = indexesAsId.indexOf(id)
+        indexesAsId.remove(id)
+        indexesAsId.add(floorMod(i + n, indexesAsId.size), id)
+    }
+
+    val izero = indexesAsId.indexOf(encryptedMessage.indexOf(0))
+    val x = encryptedMessage[indexesAsId[(izero + 1000) % indexesAsId.size]]
+    val y = encryptedMessage[indexesAsId[(izero + 2000) % indexesAsId.size]]
+    val z = encryptedMessage[indexesAsId[(izero + 3000) % indexesAsId.size]]
+
+    return x + y + z
 }
 
-fun part2(input: String): Int {
-    throw NotImplementedError()
+fun part2(input: String): Long {
+    val key = 811589153L
+    val encryptedMessage: List<Long> = input.trimEnd().lines().map(String::toLong).map { it * key }
+
+    val indexesAsId = encryptedMessage.indices.toMutableList()
+
+    repeat(10) {
+        encryptedMessage.forEachIndexed { id, n ->
+            val i = indexesAsId.indexOf(id)
+            indexesAsId.remove(id)
+            indexesAsId.add(floorMod(i + n, indexesAsId.size), id)
+        }
+    }
+
+    val izero = indexesAsId.indexOf(encryptedMessage.indexOf(0))
+    val x = encryptedMessage[indexesAsId[(izero + 1000) % indexesAsId.size]]
+    val y = encryptedMessage[indexesAsId[(izero + 2000) % indexesAsId.size]]
+    val z = encryptedMessage[indexesAsId[(izero + 3000) % indexesAsId.size]]
+
+    return x + y + z
 }
--- 2022/project/src/test/kotlin/Day20KtTest.kt
+++ 2022/project/src/test/kotlin/Day20KtTest.kt
@@ -6,19 +6,25 @@ import org.junit.jupiter.api.Test
 class Day20KtTest {
     private val testInput =
         """
-
+            1
+            2
+            -3
+            3
+            -2
+            0
+            4
         """.trimIndent()
     val myInput = this::class.java.classLoader.getResource("day20.txt")!!.readText()
 
     @Test
-    fun `test part 1 with example`() = assertEquals(33, day20.part1(testInput))
+    fun `test part 1 with example`() = assertEquals(3, day20.part1(testInput))
 
     @Test
-    fun `test part 1 with my input`() = assertEquals(1719, day20.part1(myInput))
+    fun `test part 1 with my input`() = assertEquals(7153, day20.part1(myInput))
 
     @Test
-    fun `test part 2 with example`() = assertEquals(3472, day20.part2(testInput))
+    fun `test part 2 with example`() = assertEquals(1623178306L, day20.part2(testInput))
 
     @Test
-    fun `test part 2 with my input`() = assertEquals(19530, day20.part2(myInput))
+    fun `test part 2 with my input`() = assertEquals(6146976244822, day20.part2(myInput))
 }
