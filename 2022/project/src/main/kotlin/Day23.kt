33be4322880cf454b0f3222b684847adca72df84
ricardoamendoeira <ricardo.filipe.amendoeira+github@gmail.com>
2022-12-23 09:58:00 +0000

    Add day 23

--- 2022/project/src/main/kotlin/Day23.kt
+++ 2022/project/src/main/kotlin/Day23.kt
@@ -1,9 +1,71 @@
 package day23
 
+data class Pos(val x: Int, val y: Int) {
+    fun desiredMove(elfPositions: Set<Pos>, round: Int): Pos {
+        if(neighbours().none { elfPositions.contains(it) }) return this
+
+        proposals.indices.forEach { i ->
+            val proposed = proposals[(i + round) % proposals.size](elfPositions)
+            if (proposed != this ) return proposed
+        }
+        return this
+    }
+
+    private val proposals: Array<(Set<Pos>) -> Pos> = arrayOf(
+        { elfPositions -> if ((-1..1).none { elfPositions.contains(Pos(x + it, y - 1)) }) Pos(x, y - 1) else this },
+        { elfPositions -> if ((-1..1).none { elfPositions.contains(Pos(x + it, y + 1)) }) Pos(x, y + 1) else this },
+        { elfPositions -> if ((-1..1).none { elfPositions.contains(Pos(x - 1, y + it)) }) Pos(x - 1, y) else this },
+        { elfPositions -> if ((-1..1).none { elfPositions.contains(Pos(x + 1, y + it)) }) Pos(x + 1, y) else this },
+    )
+
+    private fun neighbours(): List<Pos> {
+        return (-1..1).flatMap { dy ->
+            (-1..1).mapNotNull { dx -> if (dx != 0 || dy != 0) Pos(x+dx, y+dy) else null }
+        }
+    }
+}
+
 fun part1(input: String): Int {
-    throw NotImplementedError()
+    var elves: Set<Pos> = input.trimEnd().lines().flatMapIndexed { y, line ->
+        line.toList().mapIndexedNotNull { x, value ->
+            if(value == '#') Pos(x, y) else null
+        }
+    }.toSet()
+
+    repeat(10) { round ->
+        val proposedMovesToFrom: MutableMap<Pos, Pos?> = mutableMapOf()
+        elves.forEach { proposedMovesToFrom.merge(it.desiredMove(elves, round), it) { _, _ -> null } }
+
+        val newPositions = elves.filterNot { proposedMovesToFrom.values.contains(it) }.toMutableSet()
+        newPositions.addAll(proposedMovesToFrom.keys)
+
+        elves = newPositions
+    }
+
+    val size = (1 + elves.maxOf { it.y } - elves.minOf { it.y }) * (1 + elves.maxOf { it.x } - elves.minOf { it.x })
+    return size - elves.size
 }
 
 fun part2(input: String): Int {
-    throw NotImplementedError()
+    var elves: Set<Pos> = input.trimEnd().lines().flatMapIndexed { y, line ->
+        line.toList().mapIndexedNotNull { x, value ->
+            if(value == '#') Pos(x, y) else null
+        }
+    }.toSet()
+
+    for (round in generateSequence(0) { it + 1 }) {
+        val proposedMovesToFrom: MutableMap<Pos, Pos?> = mutableMapOf()
+        elves.forEach { proposedMovesToFrom.merge(it.desiredMove(elves, round), it) { _, _ -> null } }
+
+        val newPositions = elves.filterNot { proposedMovesToFrom.values.contains(it) }.toMutableSet()
+        newPositions.addAll(proposedMovesToFrom.keys)
+
+        if (newPositions == elves) {
+            return round + 1
+        } else {
+            elves = newPositions
+        }
+    }
+
+    throw IllegalStateException()
 }
--- 2022/project/src/test/kotlin/Day23KtTest.kt
+++ 2022/project/src/test/kotlin/Day23KtTest.kt
@@ -6,19 +6,40 @@ import org.junit.jupiter.api.Test
 class Day23KtTest {
     private val testInput =
         """
-
+            ....#..
+            ..###.#
+            #...#.#
+            .#...##
+            #.###..
+            ##.#.##
+            .#..#..
         """.trimIndent()
     val myInput = this::class.java.classLoader.getResource("day23.txt")!!.readText()
 
     @Test
-    fun `test part 1 with example`() = assertEquals(3, day23.part1(testInput))
+    fun `test part 1 with example`() = assertEquals(110, day23.part1(testInput))
 
     @Test
-    fun `test part 1 with my input`() = assertEquals(7153, day23.part1(myInput))
+    fun `test part 1 with smaller example`() {
+        val smallerExample =
+            """
+                .....
+                ..##.
+                ..#..
+                .....
+                ..##.
+                .....
+            """.trimIndent()
+        assertEquals(25, day23.part1(smallerExample))
+    }
 
     @Test
-    fun `test part 2 with example`() = assertEquals(1623178306L, day23.part2(testInput))
+    fun `test part 1 with my input`() = assertEquals(3925, day23.part1(myInput))
 
     @Test
-    fun `test part 2 with my input`() = assertEquals(6146976244822, day23.part2(myInput))
+    fun `test part 2 with example`() = assertEquals(20, day23.part2(testInput))
+
+    @Disabled // TODO: too slow
+    @Test
+    fun `test part 2 with my input`() = assertEquals(903, day23.part2(myInput))
 }
