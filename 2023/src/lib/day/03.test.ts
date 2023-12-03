973dfd6fcd9252221caf51ed4ef1e203f3fda2bb
Ricardo Amendoeira <ramendoeira@salsify.com>
2023-12-03 12:59:07 +0000

    Day 3 pt 2

--- 2023/src/lib/day/03.test.ts
+++ 2023/src/lib/day/03.test.ts
@@ -132,21 +132,26 @@ describe('part 1', () => {
 describe('part 2', () => {
     it('calculates the right value for the example', () => {
         const raw_input = `
-            Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-            Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-            Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-            Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-            Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+            467..114..
+            ...*......
+            ..35..633.
+            ......#...
+            617*......
+            .....+.58.
+            ..592.....
+            ......755.
+            ...$.*....
+            .664.598..
         `
             .replace(/^\s+/gm, '');
 
-        expect(part2(raw_input)).toBe(2286);
+        expect(part2(raw_input)).toBe(467835);
     });
 
     it('calculates the right value for the input', () => {
         const filePath = path.resolve(process.cwd(), 'static', 'inputs', '03.txt');
         const raw_input = readFileSync(filePath, 'utf8');
 
-        expect(part2(raw_input)).toBe(72706);
+        expect(part2(raw_input)).toBe(69527306);
     });
 });
--- 2023/src/lib/day/03.ts
+++ 2023/src/lib/day/03.ts
@@ -43,5 +43,44 @@ export function part1(raw_input: string): number {
 }
 
 export function part2(raw_input: string): number {
-	return 2
+	const possible_gear_numbers: possible_part[] = []
+	const gear_symbol_locations: symbol_location[] = []
+
+	raw_input.trim().split('\n').forEach((s, line) => {
+		for (const match of [...s.matchAll(/\d+/g)]) {
+			const value = Number(match[0])
+			const n_length = match[0].length
+			const column: number = <number>match.index
+			possible_gear_numbers.push({value, line, column, n_length })
+		}
+
+		for (const match of [...s.matchAll(/\*/g)]) {
+			const column: number = <number>match.index
+			gear_symbol_locations.push({line, column})
+		}
+	});
+
+	const gears: Map<symbol_location, number[]> = new Map()
+
+	possible_gear_numbers.forEach(number_location => {
+		const adjacent_to_gears = gear_symbol_locations.filter(symbol_location => {
+			const matches_columns = ((symbol_location.column >= number_location.column - 1) && (symbol_location.column <= number_location.column + number_location.n_length));
+			const matches_lines = ((symbol_location.line >= number_location.line - 1) && (symbol_location.line <= number_location.line + 1));
+
+			return matches_lines && matches_columns;
+		});
+
+		for (const gear_location of adjacent_to_gears) {
+			if (gears.has(gear_location)) {
+				gears.get(gear_location).push(number_location.value);
+			} else {
+				gears.set(gear_location, [number_location.value])
+			}
+		}
+	});
+
+	return [...gears.entries()]
+		.filter(([, numbers]) => numbers.length > 1)
+		.map(([, numbers]) => numbers.reduce((a, b) => a * b))
+		.reduce((a, b) => a + b, 0);
 }
