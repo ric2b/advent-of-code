8449448b546a064aa7c43024c053f0aa53b48ebf
Ricardo Amendoeira <ramendoeira@salsify.com>
2023-12-05 08:54:04 +0000

    Add Day 5 part 2 bruteforce attempt

--- 2023/src/lib/day/05.test.ts
+++ 2023/src/lib/day/05.test.ts
@@ -57,17 +57,49 @@ describe('part 1', () => {
 describe('part 2', () => {
     it('calculates the right value for the example', () => {
         const raw_input = `
-
+            seeds: 79 14 55 13
+            
+            seed-to-soil map:
+            50 98 2
+            52 50 48
+            
+            soil-to-fertilizer map:
+            0 15 37
+            37 52 2
+            39 0 15
+            
+            fertilizer-to-water map:
+            49 53 8
+            0 11 42
+            42 0 7
+            57 7 4
+            
+            water-to-light map:
+            88 18 7
+            18 25 70
+            
+            light-to-temperature map:
+            45 77 23
+            81 45 19
+            68 64 13
+            
+            temperature-to-humidity map:
+            0 69 1
+            1 0 69
+            
+            humidity-to-location map:
+            60 56 37
+            56 93 4
         `
-            .replace(/^\s+/gm, '');
+            .replace(/^ +/gm, '');
 
-        expect(part2(raw_input)).toBe(0);
+        expect(part2(raw_input)).toBe(46);
     });
 
     it('calculates the right value for the input', () => {
         const filePath = path.resolve(process.cwd(), 'static', 'inputs', '05.txt');
         const raw_input = readFileSync(filePath, 'utf8');
 
-        expect(part2(raw_input)).toBe(0);
+        expect(part2(raw_input)).toBe(84206669);
     });
 });
--- 2023/src/lib/day/05.ts
+++ 2023/src/lib/day/05.ts
@@ -30,5 +30,43 @@ export function part1(raw_input: string): number {
 }
 
 export function part2(raw_input: string): number {
-	return 1;
+	const [raw_seeds, ...raw_conversions] = raw_input.trim().split('\n\n')
+
+	const seed_ranges = [...raw_seeds.match(/\d+ \d+/g)].flatMap(s => {
+		const [start, length] = s.split(' ').map(Number);
+		// return Array.from({ length }, (_, i) => start + i);
+		return { start, length };
+	})
+
+	const conversions = raw_conversions.map(raw_conversion => {
+		const [conversion_type, ...raw_ranges] = raw_conversion.split('\n')
+		return raw_ranges
+			.map(raw_range => {
+				const [destination, source, length] = [...raw_range.match(/\d+/g)].map(Number)
+				return { source, length, destination }
+			});
+	});
+
+	const locations = seed_ranges.map((seed_range, i) => {
+		console.log(`Range ${i} of ${seed_ranges.length}, length: ${seed_range.length}`);
+		let min_location_for_range = Infinity
+
+		for (let seed = seed_range.start; seed < seed_range.start + seed_range.length; seed++) {
+			let current_value = seed;
+			for(const conversion of conversions) {
+				const matched_conversion = conversion.find(c => c.source <= current_value && current_value < c.source + c.length)
+
+				if (matched_conversion) {
+					current_value = current_value + (matched_conversion.destination - matched_conversion.source)
+				}
+			}
+			if (current_value < min_location_for_range) {
+				min_location_for_range = current_value;
+			}
+		}
+
+		return min_location_for_range;
+	})
+
+	return Math.min(...locations);
 }
