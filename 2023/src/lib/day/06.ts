b41037aaa37d0d733cd857abbe357f2339929bf6
Ricardo Amendoeira <ramendoeira@salsify.com>
2023-12-06 13:32:38 +0000

    Refactor Day 6

--- 2023/src/lib/day/06.ts
+++ 2023/src/lib/day/06.ts
@@ -5,16 +5,7 @@ export function part1(raw_input: string): number {
 	const records = [...raw_records.match(/\d+/g)].map(Number)
 
 	return times.map((race_time, i) => {
-		const record = records[i];
-
-		let ways = 0;
-		for(let hold_time= 1; hold_time < race_time; hold_time++) {
-			const distance = hold_time * (race_time - hold_time);
-			if (distance > record) {
-				ways++
-			}
-		}
-		return ways;
+		return ways_to_beat_record(race_time, records[i]);
 	}).reduce((a, b) => a * b);
 }
 
@@ -24,6 +15,10 @@ export function part2(raw_input: string): number {
 	const race_time = Number([...raw_time.match(/\d+/g)].join(''))
 	const record = Number([...raw_record.match(/\d+/g)].join(''))
 
+	return ways_to_beat_record(race_time, record);
+}
+
+function ways_to_beat_record(race_time: number, record: number): number {
 	let ways = 0;
 	for(let hold_time= 1; hold_time < race_time; hold_time++) {
 		const distance = hold_time * (race_time - hold_time);
