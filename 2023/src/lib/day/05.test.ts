d87643725fe4cf7ed4f1e0b4dc78dab569cd6799
Ricardo Amendoeira <ramendoeira@salsify.com>
2023-12-10 15:43:29 +0000

    [WIP] Optimize day 5 pt 2

--- 2023/src/lib/day/05.test.ts
+++ 2023/src/lib/day/05.test.ts
@@ -5,7 +5,7 @@ import path from 'path';
 import { part1, part2 } from './05';
 
 describe('part 1', () => {
-	it('calculates the right value for the example', () => {
+	it('calculates the right value for example 1', () => {
 		const raw_input = `
             seeds: 79 14 55 13
             
@@ -92,6 +92,88 @@ describe('part 2', () => {
         `.replace(/^ +/gm, '');
 
 		expect(part2(raw_input)).toBe(46);
+	});
+
+
+	it('calculates the right value for custom example 1', () => {
+		const raw_input = `
+            seeds: 13 1 14 1 55 1 79 1
+            
+            seed-to-soil map:
+            50 98 2
+            52 50 48
+        `.replace(/^ +/gm, '');
+
+		expect(part2(raw_input)).toBe(13);
+	});
+
+	it('calculates the right value for custom example 2', () => {
+		const raw_input = `
+            seeds: 13 1 14 1 55 1 79 1
+            
+            seed-to-soil map:
+            50 98 2
+            52 50 48
+            
+            soil-to-fertilizer map:
+            0 15 37
+            37 52 2
+            39 0 15
+        `.replace(/^ +/gm, '');
+
+		// 13 -> 13 -> 52
+		// 14 -> 14 -> 53
+		// 55 -> 57 -> 57
+		// 79 -> 81 -> 81
+
+		expect(part2(raw_input)).toBe(52);
+	});
+
+	it('calculates the right value for custom example 3', () => {
+		const raw_input = `
+            seeds: 13 1 14 1 55 1 79 1
+            
+            seed-to-soil map:
+            50 98 2
+            52 50 48
+            
+            soil-to-fertilizer map:
+            0 15 37
+            37 52 2
+            39 0 15
+            
+			fertilizer-to-water map:
+            49 53 8
+            0 11 42
+            42 0 7
+            57 7 4
+        `.replace(/^ +/gm, '');
+
+		// 13 -> 13 -> 52 -> 41
+		// 14 -> 14 -> 53 -> 49
+		// 55 -> 57 -> 57 -> 53
+		// 79 -> 81 -> 81 -> 81
+
+		expect(part2(raw_input)).toBe(41);
+	});
+
+	it('calculates the right value for custom example 4', () => {
+		const raw_input = `
+            seeds: 15 3 25 3
+            
+            seed-to-soil map:
+            20 10 9
+            10 20 9
+            
+            soil-to-fertilizer map:
+            20 10 9
+            10 20 9
+        `.replace(/^ +/gm, '');
+
+		// 15 -> 25 -> 15
+		// 25 -> 15 -> 25
+
+		expect(part2(raw_input)).toBe(15);
 	});
 
 	it('calculates the right value for the input', () => {
--- 2023/src/lib/day/05.ts
+++ 2023/src/lib/day/05.ts
@@ -1,28 +1,20 @@
+interface range {
+	start: number,
+	end: number,
+	offset: number
+}
+
 export function part1(raw_input: string): number {
-	// const [seeds, seed_to_soil, soil_to_fertilizer, fertilizer_to_water, water_to_light, light_to_temperature, temperature_to_humidity, humidity_to_location] = raw_input.trim().split('\n\n')
 	const [raw_seeds, ...raw_conversions] = raw_input.trim().split('\n\n');
-
 	const seeds = [...raw_seeds.match(/\d+/g)].map(Number);
-
-	const conversions = raw_conversions.map((raw_conversion) => {
-		const [, ...raw_ranges] = raw_conversion.split('\n');
-		return raw_ranges.map((raw_range) => {
-			const [destination, source, length] = [...raw_range.match(/\d+/g)].map(Number);
-			return { source, length, destination };
-		});
-	});
+	const conversions = raw_conversions.map(parse_conversion);
 
 	const locations = seeds.map((seed) => {
 		let current_value = seed;
 		for (const conversion of conversions) {
-			const matched_conversion = conversion.find(
-				(c) => c.source <= current_value && current_value < c.source + c.length
-			);
+			const matched_conversion = conversion.find((c) => c.start <= current_value && current_value < c.end);
 
-			if (matched_conversion) {
-				current_value =
-					current_value + (matched_conversion.destination - matched_conversion.source);
-			}
+			if (matched_conversion) { current_value = current_value + matched_conversion.offset; }
 		}
 		return current_value;
 	});
@@ -32,42 +24,64 @@ export function part1(raw_input: string): number {
 
 export function part2(raw_input: string): number {
 	const [raw_seeds, ...raw_conversions] = raw_input.trim().split('\n\n');
-
 	const seed_ranges = [...raw_seeds.match(/\d+ \d+/g)].flatMap((s) => {
 		const [start, length] = s.split(' ').map(Number);
-		return { start, length };
+		return { start, end: start + length - 1, offset: 0 };
 	});
 
-	const conversions = raw_conversions.map((raw_conversion) => {
-		const [, ...raw_ranges] = raw_conversion.split('\n');
-		return raw_ranges.map((raw_range) => {
-			const [destination, source, length] = [...raw_range.match(/\d+/g)].map(Number);
-			return { source, length, destination };
-		});
-	});
-
-	conversions.reverse();
-
-	for (let i = 0; ; i++) {
-		let current_value = i;
-		for (const conversion of conversions) {
-			const matched_conversion = conversion.find(
-				(c) => c.destination <= current_value && current_value < c.destination + c.length
-			);
-
-			if (matched_conversion) {
-				current_value =
-					current_value - (matched_conversion.destination - matched_conversion.source);
-			}
-		}
-
-		for (const seed_range of seed_ranges) {
-			if (
-				seed_range.start <= current_value &&
-				current_value <= seed_range.start + seed_range.length
-			) {
-				return i;
-			}
-		}
+	const conversions = raw_conversions.map(parse_conversion);
+	let ranges = seed_ranges;
+	for (const level of conversions) {
+		ranges = new_ranges_after_conversion(ranges, level);
 	}
+
+	const locations = ranges.map(range => range.start + range.offset)
+
+	return Math.min(...locations);
+}
+
+function new_ranges_after_conversion(ranges: range[], conversions: range[]): range[] {
+	const new_ranges = [];
+
+	new_ranges.push(...ranges.flatMap(r => {
+		const overlaping_conversions = conversions.filter(c => overlap(r, c));
+
+		if (overlaping_conversions.length == 0) {
+			return [r];
+		}
+
+		const split_ranges = []
+
+		const min_start: number = Math.min(...overlaping_conversions.map(c => c.start)) - r.offset;
+		const max_end: number = Math.max(...overlaping_conversions.map(c => c.end)) - r.offset;
+
+		if (r.start < min_start) {
+			split_ranges.push({start: r.start, end: min_start, offset: r.offset});
+		}
+		if (r.end > max_end) {
+			split_ranges.push({start: max_end, end: r.end, offset: r.offset});
+		}
+
+		overlaping_conversions.map(c => {
+			const overlap_start = Math.max(r.start, c.start - r.offset);
+			const overlap_end = Math.min(r.end, c.end - r.offset);
+			split_ranges.push({start: overlap_start, end: overlap_end, offset: r.offset + c.offset})
+		});
+
+		return split_ranges;
+	}));
+
+	return new_ranges;
+}
+
+function overlap(range: range,  conversion: range) {
+	return (range.start + range.offset) <= conversion.end && (range.end + range.offset) >= conversion.start
+}
+
+function parse_conversion(raw_conversion): range[] {
+	const [, ...raw_ranges] = raw_conversion.split('\n');
+	return raw_ranges.map((raw_range) => {
+		const [destination, start, length] = [...raw_range.match(/\d+/g)].map(Number);
+		return { start, end: start + length, offset: destination - start };
+	});
 }
