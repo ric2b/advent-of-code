35b9ad4c78492d2933459954812d478964679471
Ricardo Amendoeira <ramendoeira@salsify.com>
2023-12-11 08:42:57 +0000

    Add Day 11 pt 2

--- 2023/src/lib/day/11.test.ts
+++ 2023/src/lib/day/11.test.ts
@@ -45,13 +45,31 @@ describe('part 2', () => {
 			#...#.....
         `.replace(/^\t+/gm, '');
 
-		expect(part2(raw_input)).toBe(8410);
+		expect(part2(raw_input, 10)).toBe(1030);
 	});
 
+	it('calculates the right value for example 2', () => {
+		const raw_input = `
+			...#......
+			.......#..
+			#.........
+			..........
+			......#...
+			.#........
+			.........#
+			..........
+			.......#..
+			#...#.....
+        `.replace(/^\t+/gm, '');
+
+		expect(part2(raw_input, 100)).toBe(8410);
+	});
+
+	// 9486878 too low
 	it('calculates the right value for the input', () => {
 		const filePath = path.resolve(process.cwd(), 'static', 'inputs', '11.txt');
 		const raw_input = readFileSync(filePath, 'utf8');
 
-		expect(part2(raw_input)).toBe(349);
+		expect(part2(raw_input, 1_000_000)).toBe(678728808158);
 	});
 });
--- 2023/src/lib/day/11.ts
+++ 2023/src/lib/day/11.ts
@@ -4,33 +4,40 @@ interface location {
 }
 
 export function part1(raw_input: string): number {
-	const grid: string[] = raw_input.trim().split('\n');
+	return part2(raw_input, 2);
+}
 
-	const expanded_universe = expand(grid)
+export function part2(raw_input: string, expansion_factor: number = 1): number {
+	const universe: string[] = raw_input.trim().split('\n');
 
-	const galaxy_locations = []
-	for (let row = 0; row < expanded_universe.length; row++) {
-		for (let col = 0; col < expanded_universe[0].length; col++) {
-			if (expanded_universe[row][col] == '#') {
-				galaxy_locations.push({row, col})
+	const empty_rows = empty_row_indexes(universe);
+	const empty_columns = empty_column_indexes(universe);
+
+	const galaxy_locations = [];
+	for (let row = 0; row < universe.length; row++) {
+		for (let col = 0; col < universe[0].length; col++) {
+			if (universe[row][col] == '#') {
+				galaxy_locations.push({ row, col });
 			}
 		}
 	}
 
-	const distances: number[] = []
-	for(let i = 0; i < galaxy_locations.length; i++) {
-		for(let j = i + 1; j < galaxy_locations.length; j++) {
-			distances.push(manhattan_distance(galaxy_locations[i], galaxy_locations[j]));
+	const distances: number[] = [];
+	for (let i = 0; i < galaxy_locations.length; i++) {
+		for (let j = i + 1; j < galaxy_locations.length; j++) {
+			const distance = expanded_manhattan_distance(
+				empty_rows,
+				empty_columns,
+				expansion_factor,
+				galaxy_locations[i],
+				galaxy_locations[j]
+			);
+
+			distances.push(distance);
 		}
 	}
 
-	// const distances = dijkstra(grid, start_location);
-	// return Math.max(...distances.values());
 	return distances.reduce((a, b) => a + b);
-}
-
-export function part2(raw_input: string): number {
-	return 2;
 }
 
 // export function filtered_grid(raw_input: string): string[] {
@@ -72,126 +79,54 @@ export function part2(raw_input: string): number {
 // 	return filtered_grid;
 // }
 
-function manhattan_distance(a: location, b: location): number {
-	return Math.abs(a.row - b.row) + Math.abs(a.col - b.col);
+function expanded_manhattan_distance(
+	empty_rows,
+	empty_cols,
+	expansion_factor,
+	a: location,
+	b: location
+): number {
+	const empty_rows_between = empty_rows.filter(
+		(row) => row > Math.min(a.row, b.row) && row < Math.max(a.row, b.row)
+	).length;
+	const empty_cols_between = empty_cols.filter(
+		(col) => col > Math.min(a.col, b.col) && col < Math.max(a.col, b.col)
+	).length;
+	const vertical_expansion = expansion_factor * empty_rows_between - empty_rows_between;
+	const horizontal_expansion = expansion_factor * empty_cols_between - empty_cols_between;
+
+	return (
+		Math.abs(a.row - b.row) + Math.abs(a.col - b.col) + vertical_expansion + horizontal_expansion
+	);
 }
 
-function expand(grid: string[]): string[] {
+function empty_row_indexes(universe: string[]): number[] {
+	const empty_rows = [];
 
-	const empty_columns = new Array(grid[0].length).fill(true);
-	// TODO: might be slow to not iterate by row
-	for (let col = 0; col < grid[0].length; col++) {
-		for (let row = 0; row < grid.length; row++) {
-			if (grid[row][col] == '#') {
-				empty_columns[col] = false;
+	for (let row = 0; row < universe.length; row++) {
+		if (!universe[row].includes('#')) {
+			empty_rows.push(row);
+		}
+	}
+
+	return empty_rows;
+}
+
+function empty_column_indexes(universe: string[]): number[] {
+	const empty_columns = [];
+
+	for (let col = 0; col < universe[0].length; col++) {
+		let empty_column = true;
+		for (let row = 0; row < universe.length; row++) {
+			if (universe[row][col] == '#') {
+				empty_column = false;
 				break;
 			}
 		}
-	}
-
-	const horizontally_expanded = []
-	for (let row = 0; row < grid.length; row++) {
-		horizontally_expanded.push([...grid[row]].flatMap((char, col) => empty_columns[col] ? [char, char] : [char]).join(''));
-	}
-
-	return horizontally_expanded.flatMap(row => row.includes('#') ? row : [row, row]);
-}
-
-function dijkstra(grid: string[], start: location) {
-	const unvisited: Map<string, number> = new Map();
-	const distances: Map<string, number> = new Map();
-
-	distances.set(key(start), 0);
-	unvisited.set(key(start), 0);
-
-	while (unvisited.size > 0) {
-		const [node_key] = Array.from(unvisited.entries()).reduce((keyOfMinValue, [key, value]) => {
-			return value < unvisited.get(keyOfMinValue) ? key : keyOfMinValue;
-		});
-		unvisited.delete(node_key);
-		const [row, col] = node_key.split(',').map(Number);
-		const node = { row, col };
-
-		const distanceFromStart = distances.get(node_key) + 1;
-		for (const neighbor of getNeighbors(grid, node)) {
-			const neighbor_key = key(neighbor);
-			if (distances.has(neighbor_key)) {
-				if (distanceFromStart < distances.get(neighbor_key)) {
-					distances.set(neighbor_key, distanceFromStart);
-				}
-			} else {
-				unvisited.set(neighbor_key, distanceFromStart);
-				distances.set(neighbor_key, distanceFromStart);
-			}
+		if (empty_column) {
+			empty_columns.push(col);
 		}
 	}
 
-	return distances;
-}
-
-function key(location: location): string {
-	return `${location.row},${location.col}`;
-}
-
-function getNeighbors(grid: string[], location: location): location[] {
-	let candidates: location[];
-	switch (grid[location.row][location.col]) {
-		case '|':
-			candidates = [
-				{ row: location.row - 1, col: location.col },
-				{ row: location.row + 1, col: location.col }
-			];
-			break;
-		case '-':
-			candidates = [
-				{ row: location.row, col: location.col - 1 },
-				{ row: location.row, col: location.col + 1 }
-			];
-			break;
-		case 'L':
-			candidates = [
-				{ row: location.row - 1, col: location.col },
-				{ row: location.row, col: location.col + 1 }
-			];
-			break;
-		case 'J':
-			candidates = [
-				{ row: location.row - 1, col: location.col },
-				{ row: location.row, col: location.col - 1 }
-			];
-			break;
-		case '7':
-			candidates = [
-				{ row: location.row + 1, col: location.col },
-				{ row: location.row, col: location.col - 1 }
-			];
-			break;
-		case 'F':
-			candidates = [
-				{ row: location.row + 1, col: location.col },
-				{ row: location.row, col: location.col + 1 }
-			];
-			break;
-		case 'S':
-			candidates = [];
-			if (['|', '7', 'F'].includes(grid[location.row - 1]?.[location.col]))
-				candidates.push({ row: location.row - 1, col: location.col });
-			if (['|', 'J', 'L'].includes(grid[location.row + 1]?.[location.col]))
-				candidates.push({ row: location.row + 1, col: location.col });
-			if (['-', 'F', 'L'].includes(grid[location.row]?.[location.col - 1]))
-				candidates.push({ row: location.row, col: location.col - 1 });
-			if (['-', '7', 'J'].includes(grid[location.row]?.[location.col + 1]))
-				candidates.push({ row: location.row, col: location.col + 1 });
-			break;
-		default:
-			candidates = [];
-	}
-
-	return inside_grid(grid, candidates);
-}
-
-function inside_grid(grid: string[], locations: location[]): location[] {
-	const rows = grid.length;
-	const cols = grid[0].length;
-	return locations.filter((l) => l.row >= 0 && l.row < rows && l.col >= 0 && l.col < cols);
+	return empty_columns;
 }
