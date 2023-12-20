79e570001fa7faa919ce562a5ed0715eeee476de
Ricardo Amendoeira <ramendoeira@salsify.com>
2023-12-20 16:50:58 +0000

    Mark uncomplete days as TBD

---
+++ 2023/src/lib/day/17.svelte
@@ -0,0 +1,35 @@
+<script>
+	export let raw_input;
+
+	// import { part1, part2, part_1_grid } from './17';
+	//
+	// $: p1 = part1(raw_input);
+	// $: p2 = part2(raw_input);
+	//
+	// $: grid = part_1_grid(raw_input);
+</script>
+
+<p>Part 1: TBD</p>
+<p>Part 2: TBD</p>
+
+<!--<p class="aoc_yellow">Part 1: {p1}</p>-->
+<!--<p class="aoc_yellow">Part 2: {p2}</p>-->
+
+<!--<div class="grid">-->
+<!--	{#each grid as row}-->
+<!--		<div class="row">{row}</div>-->
+<!--	{/each}-->
+<!--</div>-->
+
+<!--<style>-->
+<!--	.grid {-->
+<!--		font-family: monospace;-->
+<!--		font-size: 1ch;-->
+<!--		line-height: 1ch;-->
+<!--		white-space: pre;-->
+<!--		color: red;-->
+<!--	}-->
+
+<!--	.row {-->
+<!--	}-->
+<!--</style>-->
