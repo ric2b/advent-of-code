6475fe2b41ebede9660c0c8dc2d17e735940e91f
Ricardo Amendoeira <ramendoeira@salsify.com>
2023-12-21 16:22:45 +0000

    Add Day 21 pt 2

--- 2023/src/lib/day/21.test.ts
+++ 2023/src/lib/day/21.test.ts
@@ -2,7 +2,7 @@ import { describe, it, expect } from 'vitest';
 import { readFileSync } from 'fs';
 import path from 'path';
 
-import {part1, part2} from './21';
+import {part1, part2, part2b} from './21';
 
 describe('part 1', () => {
 	it('calculates the right value for example 1', () => {
@@ -34,16 +34,32 @@ describe('part 1', () => {
 describe('part 2', () => {
 	it('calculates the right value for example 1', () => {
 		const raw_input = `
-
+			...........
+			.....###.#.
+			.###.##..#.
+			..#.#...#..
+			....#.#....
+			.##..S####.
+			.##..#...#.
+			.......##..
+			.##.#.####.
+			.##..##.##.
+			...........
 		`.replace(/^[ \t]+/gm, '');
 
-		expect(part2(raw_input)).toBe(1);
+		expect(part2(raw_input)).toBe(391791381003720);
 	});
 
 	it('calculates the right value for the input', () => {
 		const filePath = path.resolve(process.cwd(), 'static', 'inputs', '21.txt');
 		const raw_input = readFileSync(filePath, 'utf8');
 
-		expect(part2(raw_input)).toBe(1);
+		// 21628988986748133000 too high!
+		// 9635477433421572000 too high!
+		// 151104306597970460 not right
+		// 151102812745422370 not right
+		// 78636785046373 too low
+
+		expect(part2(raw_input)).toBe(627960775905777);
 	});
 });
--- 2023/src/lib/day/21.ts
+++ 2023/src/lib/day/21.ts
@@ -1,7 +1,3 @@
-import {vi} from "vitest";
-import * as wasi from "wasi";
-import type {Loc} from "eslint-plugin-svelte/lib/shared/svelte-compile-warns";
-
 export function part1(raw_input: string, step_limit: number = 64): number {
 	const grid: number[][] = raw_input.trim().split('\n').map((line) => line.split(''));
 
@@ -22,10 +18,92 @@ export function part1(raw_input: string, step_limit: number = 64): number {
 	return reachable_at_step_limit.size;
 }
 
-export function part2(raw_input: string): number {
-	raw_input.trim().split('\n')
+export function part2(raw_input: string, step_limit: number = 64): number {
+	const grid: number[][] = raw_input.trim().split('\n').map((line) => line.split(''));
+	const side = grid.length;
 
-	return 2;
+	const expanded_grid = Array.from(Array(grid.length * 5), (_, i) => {
+		const row = i % grid.length;
+		return grid[row].concat(...grid[row], ...grid[row], ...grid[row], ...grid[row]);
+	});
+
+	const starts = [];
+	for(let i = 0; i < expanded_grid.length; i++) {
+		for(let j = 0; j < expanded_grid[0].length; j++) {
+			if(expanded_grid[i][j] == 'S') {
+				starts.push(new Location(j, i));
+			}
+		}
+	}
+
+	// Q: how many squares at 26501365 steps?
+
+	// row and column for S are completely empty, so the center of other regions can be reached in width steps!
+	// walking on one axis, can reach: 1 + (2 * 26501365 / 131) = 404601 regions (2 * because walking in both direction)
+	// leftover steps is 26501365 % 131 = 65
+	// RHa = 202300 regions reached in one axis means 404601**2 / 2 (rhombus area) regions reached in both axis = 81850984600 regions
+
+	// Rp = each full region has X empty plots
+	// final spaces that can be reached are RHa * Rp + permiter region count * reachable in 65 steps from edge
+
+	// 01234
+	// 56789
+	// 01234
+	// 56789
+	// 01234
+	const start = starts[12]
+	const graph = build_graph(expanded_grid, start);
+
+	const goal_steps = 26501365;
+	const mod = goal_steps % side;
+
+	const y0 = new Set(bfs(graph, start, mod).map(n => n.key)).size
+	const y1 = new Set(bfs(graph, start, mod + side).map(n => n.key)).size
+	const y2 = new Set(bfs(graph, start, mod + 2*side).map(n => n.key)).size
+	// const [y0, y1, y2] = [3877, 34674, 96159]
+
+	// https://en.wikipedia.org/wiki/Lagrange_polynomial#Example
+	// Lagrange's Interpolation formula for ax^2 + bx + c with x=[0,1,2] and y=[y0,y1,y2]
+	// f(x) = (x^2-3x+2) * y0/2 - (x^2-2x)*y1 + (x^2-x) * y2/2
+
+	// x = 0
+	// c = 2*y0/2
+	// c = y0
+
+	// x = 1
+	// a1^2 + b1 + c = (1-3+2) * y0/2 - (1-2)*y1 + (1-1) * y2/2
+	// a + b + c = y1
+	// a + b + y0 = y1
+	// a + b = y1 - y0
+	// b = (y1 - y0) - a
+	// b = (y1 - y0) - (y0/2 - y1 + y2/2)
+	// b = y1 - y0 - y0/2 + y1 - y2/2
+	// b = -3y0/2 + 2y1 - y2/2
+
+	// x = 2
+	// 4a + 2b + c = (2^2-3*2+2) * y0/2 - (2^2-2*2)*y1 + (2^2-2) * y2/2
+	// 4a + 2b + c = (4-6+2) * y0/2 - (4-4)*y1 + (4-2) * y2/2
+	// 4a + 2b + y0 = y2
+	// 4a + 2b = y2 - y0
+	// 2a + b = y2/2 - y0/2
+	// 2a + (y1 - y0) - a = y2/2 - y0/2
+	// a + (y1 - y0) = y2/2 - y0/2
+	// a = y2/2 - y0/2 - (y1 - y0)
+	// a = y2/2 - y0/2 - y1 + y0
+	// a = y0/2 - y1 + y2/2
+
+	/////////////////////////
+	// a = y0/2 - y1 + y2/2
+	// b = -3y0/2 + 2y1 - y2/2
+	// c = y0
+
+	const coeff_a = y0/2 - y1 + y2/2;
+	const coeff_b = -3*(y0/2) + 2*y1 - y2/2;
+	const coeff_c = y0;
+	// const [coeff_a, coeff_b, coeff_c] = [15344, 15453, 3877];
+
+	const N= (goal_steps - mod) / side
+	return  coeff_a * N**2 + (coeff_b * N) + coeff_c;
 }
 
 class Location {
