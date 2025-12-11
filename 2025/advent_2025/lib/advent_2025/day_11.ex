4c338606a163be7a7abc1a1653c3f1a04a031880
Ricardo Amendoeira <ramendoeira@salsify.com>
2025-12-11 09:25:17 +0000

    Day 11 pt 2

--- 2025/advent_2025/lib/advent_2025/day_11.ex
+++ 2025/advent_2025/lib/advent_2025/day_11.ex
@@ -1,4 +1,6 @@
 defmodule Advent2025.Day11 do
+  use Memoize
+
   @moduledoc """
   Solution for Advent of Code 2025 - Day 11
   """
@@ -12,33 +14,43 @@ defmodule Advent2025.Day11 do
   @doc """
   Returns the example input for the given part (1 or 2).
   """
-  def example_input(_part) do
-    """
-    aaa: you hhh
-    you: bbb ccc
-    bbb: ddd eee
-    ccc: ddd eee fff
-    ddd: ggg
-    eee: out
-    fff: out
-    ggg: out
-    hhh: ccc fff iii
-    iii: out
-    """
+  def example_input(part) do
+    case part do
+      1 -> """
+           aaa: you hhh
+           you: bbb ccc
+           bbb: ddd eee
+           ccc: ddd eee fff
+           ddd: ggg
+           eee: out
+           fff: out
+           ggg: out
+           hhh: ccc fff iii
+           iii: out
+           """
+      2 -> """
+           svr: aaa bbb
+           aaa: fft
+           fft: ccc
+           bbb: tty
+           tty: ccc
+           ccc: ddd eee
+           ddd: hub
+           hub: fff
+           eee: dac
+           dac: fff
+           fff: ggg hhh
+           ggg: out
+           hhh: out
+           """
+      end
   end
 
   @doc """
   Solves part 1 of the puzzle.
   """
   def part1(input) do
-    graph = input
-    |> String.split("\n", trim: true)
-    |> Enum.map(fn line ->
-      [from, to] = String.split(line, ": ")
-      {from, String.split(to, " ")}
-    end)
-    |> Map.new()
-
+    graph = parse_graph(input)
     count_paths_with_dfs(graph, "you", "out")
   end
 
@@ -46,13 +58,31 @@ defmodule Advent2025.Day11 do
   Solves part 2 of the puzzle.
   """
   def part2(input) do
-    input
+    graph = parse_graph(input)
+
+    # [count_paths_with_dfs(graph, "dac", "fft"), count_paths_with_dfs(graph, "fft", "dac")]
+    # (count_paths_with_dfs(graph, "svr", "dac") *
+    # count_paths_with_dfs(graph, "dac", "fft") *
+    # count_paths_with_dfs(graph, "fft", "out")) +
+    (count_paths_with_dfs(graph, "svr", "fft") *
+    count_paths_with_dfs(graph, "fft", "dac") *
+    count_paths_with_dfs(graph, "dac", "out"))
   end
 
-  defp count_paths_with_dfs(_graph, start, target) when start == target, do: 1
-  defp count_paths_with_dfs(graph, start, target) do
-    graph[start]
+  defmemop count_paths_with_dfs(_graph, start, target) when start == target, do: 1
+  defmemop count_paths_with_dfs(graph, start, target) do
+    Map.get(graph, start, [])
     |> Enum.map(fn neighbor -> count_paths_with_dfs(graph, neighbor, target) end)
-    |> Enum.sum()
+    |> Enum.reduce(0, fn count, acc -> count + acc end)
+  end
+
+  defp parse_graph(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      [from, to] = String.split(line, ": ")
+      {from, String.split(to, " ")}
+    end)
+    |> Map.new()
   end
 end
--- 2025/advent_2025/test/advent_2025/day_11_test.exs
+++ 2025/advent_2025/test/advent_2025/day_11_test.exs
@@ -13,14 +13,13 @@ defmodule Advent2025.Day11Test do
   end
 
   describe "part2" do
-    @tag :skip
     test "solves sample input" do
-      assert @day.part2(@day.example_input(2)) == :todo
+      assert @day.part2(@day.example_input(2)) == 2
     end
 
-    @tag :skip
+    @tag timeout: :infinity
     test "solves actual input" do
-      assert @day.part2(@actual_input) == :todo
+      assert @day.part2(@actual_input) == 413167078187872
     end
   end
 end
