08af8180521cb09cc2a22b23f56537f8c8217c87
Ricardo Amendoeira <ramendoeira@salsify.com>
2025-12-14 19:29:19 +0000

    Fix grid columns on large viewports

--- 2025/advent_2025/lib/advent_2025_web/live/home_live.ex
+++ 2025/advent_2025/lib/advent_2025_web/live/home_live.ex
@@ -63,7 +63,7 @@ defmodule Advent2025Web.HomeLive do
           </div>
 
           <%!-- Days Grid --%>
-          <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+          <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-4">
             <%= for day <- @days do %>
               <%= if day.available do %>
                 <.link
